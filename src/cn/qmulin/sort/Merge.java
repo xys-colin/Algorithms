63919feb437af9f83fb592e47165f63f67f2a8f4
xys-colin <dana2583247805@foxmail.com>
2020-11-06 23:32:25 +0800

    自底向上的归并排序

--- src/cn/qmulin/sort/Merge.java
+++ src/cn/qmulin/sort/Merge.java
@@ -9,23 +9,21 @@ public class Merge {
     private static Comparable[] aux;
 
     public static void main(String[] args) {
-        Integer[] a={7,8,4,5,3,9,2,5,4};
-        sort(a);
-        assert  Example.isSorted(a);
-        Example.show(a);
-    }
-    public static void sort(Comparable[] a) {
+        Integer[] a = {7, 8, 4, 5, 3, 9, 2, 5, 4};
         aux = new Comparable[a.length];
         sort(a, 0, a.length - 1);
+        assert Example.isSorted(a);
+        Example.show(a);
     }
 
     /**
      * 自顶向下的归并排序
-     * @param a 数组
+     *
+     * @param a  数组
      * @param lo 开始下标
      * @param hi 结束下标
      */
-    private static void sort(Comparable[] a, int lo, int hi) {
+    public static void sort(Comparable[] a, int lo, int hi) {
         if (lo >= hi) return;
         int mid = lo + (hi - lo) / 2;
         sort(a, lo, mid);//将左半边排序
@@ -34,7 +32,21 @@ public class Merge {
     }
 
     /**
+     * 自底向上的归并排序
+     * @param a
+     */
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int sz = 1; sz < N; sz = sz + sz) {//sz数组大小
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {//lo:子数组索引
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+            }
+        }
+    }
+
+    /**
      * 原地归并的抽象方法
+     *
      * @param a
      * @param lo
      * @param mid
